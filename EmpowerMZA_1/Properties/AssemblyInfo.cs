4dac374f966ab4c16697139fd52519cbef44c10c
kordiler <marekziak@hotmail.com>
2023-03-28 14:24:29 +0200

    Add project files.

---
+++ EmpowerMZA_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerMZA_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerMZA_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6E1A94F8-2292-458A-ABC4-12DF9A11BC36")]
+[assembly: AssemblyVersion("1.0.0.0")]
